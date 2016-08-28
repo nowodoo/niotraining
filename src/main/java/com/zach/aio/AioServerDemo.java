b3b56859fe42bb672ee8afa8eeb8bf2d1c908b7b
zach ma <zach.ma@centling.com>
2016-08-28 14:46:18 +0800

    zac-7  异步的aio形式的server

---
+++ src/main/java/com/zach/aio/AioServerDemo.java
@@ -0,0 +1,74 @@
+package com.zach.aio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by Administrator on 2016-8-28.
+ */
+public class AioServerDemo {
+    public static void startServer() throws Exception {
+        AsynchronousServerSocketChannel asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open();
+        asynchronousServerSocketChannel.bind(new InetSocketAddress(8999));
+
+        //使用匿名内部类（这里的accept方法表示是有连接进来了，就是这么简单，连接上了时候就会直接触发complete这个方法）
+        asynchronousServerSocketChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
+            /**
+             * 这个方法就是有请求过来了，我们要处理它
+             * @param result
+             * @param attachment
+             */
+            @Override
+            public void completed(AsynchronousSocketChannel result, Void attachment) {
+                try {
+                    operate(result);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+
+            /**
+             * 要是连接处理失败了就会触发这个方法
+             * @param exc
+             * @param attachment
+             */
+            @Override
+            public void failed(Throwable exc, Void attachment) {
+
+            }
+        });
+
+
+    }
+
+    /**
+     * 腹泻方法的具体的实现方法
+     */
+    private static void operate(AsynchronousSocketChannel ch) throws Exception {
+        ByteBuffer buf = ByteBuffer.allocate(48);
+        //注意这里和原先相比还有一个get方法。
+        int size = ch.read(buf).get();
+
+        while (size > 0) {
+            buf.flip();
+            Charset charset = Charset.forName("UTF-8");
+            charset.newDecoder().decode(buf).toString();
+            size = ch.read(buf).get();
+        }
+
+        //最后在操作完成之后要记得关闭这个channel
+        ch.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        startServer();
+    }
+}
